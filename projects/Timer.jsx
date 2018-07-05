11d72125f1a154c78aca23a8c21f99bf8f4301df
나야나 <sss@sss.com>
2018-07-05 16:47:17 +0900

    hello

--- projects/Timer.jsx
+++ projects/Timer.jsx
@@ -14,18 +14,10 @@ class Timer extends Component {
       date: moment(),
     };
 
-    // this.nTimer = setInterval(this.forceUpdate(), 1000);
     this.nTimer = setInterval(this.handleTick, 1000);
   }
 
   handleTick = () => {
-    // console.log(this.expireDate);
-    // console.log(moment(this.props.expireDate).diff(moment()));
-    // if (moment(this.props.expireDate).diff(moment()) > 0) {
-    //   this.forceUpdate();
-    // } else {
-    //   alert('종료되었습니다');
-    // }
     this.setState({
       date: moment(),
     });
@@ -36,17 +28,11 @@ class Timer extends Component {
     console.log(nextState);
     if (this.state.date.format('mm') === nextState.date.format('mm')) {
       return false;
-    } else {
-      return true;
     }
+    return true;
   }
 
   render() {
-    // <div>
-    //   <div>현재시간은 {moment().format('LT')} 입니다</div>
-    //   <div>{moment(this.props.expireDate).fromNow()}에 강의 종료합니다.</div>
-    // </div>
-    // {moment(expireDate).diff(moment(), 'seconds')}후에 강의 종료합니다.
     const { expireDate } = this.props;
     const { date } = this.state;
     console.log('render...');
